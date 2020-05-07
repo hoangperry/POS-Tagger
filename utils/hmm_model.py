4cc88c7fb39568efd158b254f1b34e9f7c0b5619
hoangperry <prodai9573@gmail.com>
2020-05-07 20:47:36 +0700

    update viterbi

--- utils/hmm_model.py
+++ utils/hmm_model.py
@@ -2,6 +2,41 @@ import re
 import pickle
 
 
+def viterbi(_obs, _states, _s_pro, _t_pro, _e_pro):
+    # init
+    path = {
+        s: []
+        for s in states
+    }
+    curr_pro = dict()
+    for s in states:
+        curr_pro[s] = _s_pro[s] * _e_pro[s][obs[0]]
+    for i in range(1, len(obs)):
+        last_pro = curr_pro
+        curr_pro = {}
+        for curr_state in states:
+            max_pro, last_sta = max((
+                    (
+                        last_pro[last_state] * _t_pro[last_state][curr_state] * _e_pro[curr_state][obs[i]],
+                        last_state
+                    )
+                    for last_state in states
+                ), key=lambda x: x[0]
+            )
+
+            curr_pro[curr_state] = max_pro
+            path[curr_state].append(last_sta)
+
+    max_pro = -1
+    max_path = None
+    for s in states:
+        path[s].append(s)
+        if curr_pro[s] > max_pro:
+            max_path = path[s]
+            max_pro = curr_pro[s]
+    return max_path
+
+
 class HMMBasic:
     def __init__(self):
         self.num_words_train = 0
@@ -111,3 +146,53 @@ class HMMBasic:
     def load(model_path):
         model_ = pickle.load(open(model_path, 'rb'))
         return model_
+
+
+if __name__ == '__main__':
+    states = (
+        'Healthy',
+        'Fever'
+    )
+    observations = (
+        'normal',
+        'cold',
+        'dizzy'
+    )
+
+    start_probability = {
+        'Healthy': 0.6,
+        'Fever': 0.4
+    }
+
+    transition_probability = {
+        'Healthy': {'Healthy': 0.7, 'Fever': 0.3},
+        'Fever': {'Healthy': 0.4, 'Fever': 0.6},
+    }
+
+    emission_probability = {
+        'Healthy': {
+            'normal': 0.5,
+            'cold': 0.4,
+            'dizzy': 0.1
+        },
+        'Fever': {
+            'normal': 0.1,
+            'cold': 0.3,
+            'dizzy': 0.6
+        },
+    }
+
+    obs = [
+        'normal',
+        'cold',
+        'dizzy'
+    ]
+
+    resut_vtb = viterbi(
+        obs,
+        states,
+        start_probability,
+        transition_probability,
+        emission_probability
+    )
+    print(resut_vtb)
